cf6c937884aa32efc0d29c31da15e9e1a015edef
shalim-khan <shalimkhan@outlook.com>
2022-01-30 23:38:40 +0000

    Cleanup

--- flutter_first_run/lib/main.dart
+++ flutter_first_run/lib/main.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'utils.dart';
 
-var displayText = "";
+var mainText = "";
 var subText = "This app has been run ";
 var subTextEnding = "";
 
 void main() async {
+  // Initialise local storage
+
   await Hive.initFlutter();
-  await Hive.openBox(
-      'flutter-first-run-six'); // increment this to reset first run status for testing
+  await Hive.openBox('flutter-first-run-six'); // increment this to reset first run status for testing
   var box = Hive.box('flutter-first-run-six');
+
+  // Check how many times this app has been run, and increment by one
 
   var runCount = box.get('runCount', defaultValue: 0);
   runCount++;
   box.put('runCount', runCount);
 
+  // Set behaviour according to how many times the app has been run
+
   if (runCount == 1) {
-    displayText = "This is the first run";
+    mainText = "This is the first run";
     subTextEnding = "once";
   } else if (runCount == 2) {
-    displayText = "This is NOT the first run";
+    mainText = "This is NOT the first run";
     subTextEnding = "twice";
   } else {
-    displayText = "This is NOT the first run";
+    mainText = "This is NOT the first run";
     subTextEnding = "$runCount times";
   }
+
+  // Run the app itself
 
   runApp(CleanApp(home: HomePage()));
 }
@@ -38,68 +46,31 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              displayText,
-              textAlign: TextAlign.center,
-              style: GoogleFonts.lato(
-                fontSize: 25,
-              ),
-            ),
+            buildMainText(),
             SizedBox(height: 10),
-            Text(
-              subText + subTextEnding,
-              textAlign: TextAlign.center,
-              style: GoogleFonts.lato(
-                fontSize: 15,
-              ),
-            ),
+            buildSubText(),
           ],
         ),
       ),
     );
   }
-}
 
-class AnimatedRocketShip extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.white,
-          ),
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(20))),
-      padding: EdgeInsets.all(10.0),
-      child: Image.asset(
-        "images/rocket.gif",
-        height: 120,
+  Text buildMainText() {
+    return Text(
+      mainText,
+      textAlign: TextAlign.center,
+      style: GoogleFonts.lato(
+        fontSize: 25,
       ),
     );
   }
-}
 
-class CleanApp extends StatelessWidget {
-  final Widget home;
-
-  CleanApp({
-    required this.home,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: home,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors
-            .cyan, // iOS PWA status bar background color for light AND dark modes
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.grey.shade900,
+  Text buildSubText() {
+    return Text(
+      subText + subTextEnding,
+      textAlign: TextAlign.center,
+      style: GoogleFonts.lato(
+        fontSize: 15,
       ),
     );
   }
